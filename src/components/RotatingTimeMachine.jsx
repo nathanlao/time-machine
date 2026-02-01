c89dddba19c46db8b5e730c5248f1c34076d42ab
NathanLao <75557717+nathanlao@users.noreply.github.com>
2026-01-31 21:58:05 -0800

    should show alert once (#8)

--- src/components/RotatingTimeMachine.jsx
+++ src/components/RotatingTimeMachine.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useFrame } from '@react-three/fiber';
 import { TimeMachine } from './3dModel/Time_machine';
 import { gsap } from 'gsap';
